05f544b71215d8e56bf89a6df242e144700c9689
Belov <belov.boris.a@gmail.com>
2025-05-03 12:53:39 +0300

    Fix deploy

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,10 +2,8 @@ import dotenv from 'dotenv';
 
 export const BOT_TOKEN = (() => {
   if (process.env.NODE_ENV !== 'production') {
-    console.log('Running in non-prod');
     dotenv.config();
   }
-  console.log('BOT_TOKEN in env:', process.env);
   const token = process.env.BOT_TOKEN;
   if (!token) {
     throw new Error('BOT_TOKEN is not set in .env file');
