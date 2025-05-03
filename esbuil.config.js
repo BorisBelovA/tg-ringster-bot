d7d7d43b6b41cc0ea43f67bddafde9f42c19a4a3
Belov <belov.boris.a@gmail.com>
2025-05-03 13:07:46 +0300

    Fix deploy

---
+++ esbuil.config.js
@@ -0,0 +1,21 @@
+const esbuild = require('esbuild');
+const alias = require('@esbuild-plugin-alias');
+
+esbuild.build({
+  entryPoints: ['src/index.ts'],
+  bundle: true,
+  platform: 'node',
+  target: 'node18',
+  outdir: 'dist',
+  plugins: [
+    alias({
+      '@actions': require('path').resolve(__dirname, 'src/actions'),
+      '@scenes': require('path').resolve(__dirname, 'src/scenes'),
+      '@utils': require('path').resolve(__dirname, 'src/utils'),
+      '@config': require('path').resolve(__dirname, 'src/config'),
+      '@handlers': require('path').resolve(__dirname, 'src/handlers'),
+    }),
+  ],
+})
+.then(() => console.log('✅ Build complete'))
+.catch(() => process.exit(1));
