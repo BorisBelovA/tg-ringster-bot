c301776c837c50ee518d0c2bec1c634c26553e71
Belov <belov.boris.a@gmail.com>
2025-05-17 11:49:49 +0300

    Configure local and prod runs

--- src/bot.ts
+++ src/bot.ts
@@ -1,10 +1,13 @@
 import { Scenes, session, Telegraf } from 'telegraf';
-import { BOT_TOKEN } from './config/env';
+import { BOT_TOKEN, DOMAIN, IS_PROD, PORT } from './config/env';
 import { setupActions } from '@actions/index';
 import { singleReminderScene } from '@scenes/single-reminder';
 import { MyContext } from 'utils';
+import express from 'express';
 
-export function startBot() {
+const app = express();
+
+export async function startBot() {
   const bot = new Telegraf<MyContext>(BOT_TOKEN);
 
   const stage = new Scenes.Stage<MyContext>([singleReminderScene]);
@@ -12,12 +15,37 @@ export function startBot() {
   bot.use(stage.middleware());
 
   setupActions(bot);
-
-  bot.launch().then(() => {
-    console.log('🚀 Bot started');
-  });
+  startListening(bot);
 
   // Enable graceful stop
   process.once('SIGINT', () => bot.stop('SIGINT'));
   process.once('SIGTERM', () => bot.stop('SIGTERM'));
+}
+
+const startListening = async (bot: Telegraf<MyContext>) => {
+  if (IS_PROD) {
+    if (!DOMAIN) {
+      throw new Error('DOMAIN environment variable is required in production');
+    }
+    if (!PORT) {
+      throw new Error('PORT environment variable is required in production');
+    }
+    app.use(express.json()); // важно: телеграм шлёт JSON
+    app.use(bot.webhookCallback('/telegram'));
+
+    await bot.telegram.setWebhook(`https://${DOMAIN}/telegram`);
+    console.log(`✅ Webhook set to https://${DOMAIN}/telegram`);
+
+    app.get('/', (req, res) => {
+      res.send('🤖 Bot is running via webhook')
+    });
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Express server listening on port ${PORT}`);
+    });
+  } else {
+    bot.launch().then(() => {
+      console.log('🚀 Bot started locally');
+    });
+  }
 }
--- src/config/env.ts
+++ src/config/env.ts
@@ -10,3 +10,29 @@ export const BOT_TOKEN = (() => {
   }
   return token;
 })();
+
+export const PORT = (() => {
+  if (process.env.NODE_ENV !== 'production') {
+    dotenv.config();
+  }
+  const port = process.env.PORT;
+  if (!port) {
+    throw new Error('PORT is not set in .env file');
+  }
+  return port;
+})();
+
+export const IS_PROD = (() => {
+  return process.env.NODE_ENV === 'production'
+})();
+
+export const DOMAIN = (() => {
+  if (process.env.NODE_ENV !== 'production') {
+    dotenv.config();
+  }
+  const domain = process.env.DOMAIN;
+  if (!domain) {
+    throw new Error('DOMAIN is not set in .env file');
+  }
+  return domain;
+})();
